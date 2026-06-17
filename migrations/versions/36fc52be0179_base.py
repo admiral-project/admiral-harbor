66c14d09e42bad98139d765f8df2e3f8b1dc4edb
William Moreno <williamjmorenor@gmail.com>
2026-06-17 17:55:19 -0400

    style(ruff): remove unused imports in base migration
    Signed-off-by: William Moreno Reyes <williamjmorenor@gmail.com>

--- migrations/versions/36fc52be0179_base.py
+++ migrations/versions/36fc52be0179_base.py
@@ -8,8 +8,6 @@ Create Date: 2026-06-17 16:15:39.658929
 
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "36fc52be0179"
