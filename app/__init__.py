7333f7f0ed7441f80a82dd54982bb27485a93225
William Moreno Reyes <williamjmorenor@gmail.com>
2026-07-17 13:08:07 -0400

    fix(factory): remove unused SQLAlchemy import
    Signed-off-by: William Moreno Reyes <williamjmorenor@gmail.com>

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ import os
 from flask import Flask, request
 from flask_login import current_user
 from pathlib import Path
-from sqlalchemy import inspect, text
+from sqlalchemy import inspect
 import logging
 
 from app.admin import bp as admin_bp
